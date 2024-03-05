ce52c58854a197f52988cec5de5ee6a9844a2603
Ian Turner <ian96turner@gmail.com>
2024-03-04 20:49:48 -0700

    trying to deploy on WSL, not working

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,25 +1,7 @@
 use actix_cors::Cors;
-use actix_web::{web, App, post, HttpResponse, HttpServer, Responder};
+use actix_web::{App, post, HttpResponse, HttpServer, Responder};
 use actix_files as fs;
 
-async fn index() -> impl Responder {
-    let html = r#"<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://exemple.com/path/to/frame-image.jpg" />
-    <meta property="og:image" content="https:://example.com/path/to/og-image.jpg" />
-    <title>Trinket Frame</title>
-</head>
-<body>
-    <h1>Welcome to the Frame</h1>
-</body>
-</html>"#;
-
-    HttpResponse::Ok().content_type("text/html").body(html)
-}
 
 #[post("/api/interaction")]
 async fn handle_interaction() -> impl Responder {
